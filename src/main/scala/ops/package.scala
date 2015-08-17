3a0f112c69a6c05d8ead95fe6e22ed7345debba5
Anton Kolotaev <anton.kolotaev@gmail.com>
2015-08-17 19:26:25 +0200

    Conversion typeclass

--- src/main/scala/ops/package.scala
+++ src/main/scala/ops/package.scala
@@ -1,9 +1,36 @@
 package object ops
 {
 
-    trait ConversionDefined[From, To]
+    trait Conversion[From, To]
     {
         def convert(from : From) : To
+    }
+
+    object Conversions {
+
+        implicit def id[T] : Conversion[T,T] =
+            new Conversion[T,T]
+            {
+                def convert(x : T) = x
+            }
+
+        implicit val int2double = new Conversion[Int, Double]{  def convert(x : Int) = x  }
+        implicit val int2long = new Conversion[Int, Long]{  def convert(x : Int) = x  }
+        implicit val long2double = new Conversion[Long, Double]{  def convert(x : Long) = x  }
+
+        implicit def toOption[T, U](implicit ev : Conversion[T,U]) : Conversion[T, Option[U]] =
+            new Conversion[T, Option[U]]
+            {
+                def convert(x : T) = Some(ev.convert(x))
+            }
+
+        /* TODO: understand why it doesn't work
+        implicit def compose[A,B,C](implicit a2b : ConversionDefined[A,B], b2c : ConversionDefined[B,C]) : ConversionDefined[A,C] =
+            new ConversionDefined[A,C]
+            {
+                def convert(a : A) = b2c.convert(a2b.convert(a))
+            }
+            */
     }
 
     trait PlusDefined[A,B]
@@ -18,6 +45,8 @@ package object ops
             type Ret = T
             def plus(a : T, b : T) = implicitly[Numeric[T]].plus(a,b)
         }
+
+        //implicit def rightConversion[A,B : PlusDefined](implicit  ev : )
     }
 
 
---
+++ src/test/scala/ops/ConversionSpec.scala
@@ -0,0 +1,16 @@
+package ops
+
+import org.scalatest.FlatSpec
+
+class ConversionSpec extends FlatSpec {
+
+    import Conversions._
+
+    def convert[From, To](x : From)(implicit ev: Conversion[From, To]) = ev.convert(x)
+
+    "Int" should "convert to Double" in { assert(convert[Int, Double](1) == 1.0) }
+
+    "Int" should "convert to Option[Int]" in { assert(convert[Int, Option[Int]](1) == Some(1)) }
+
+    "Int" should "convert to Option[Double]" in { assert(convert[Int, Option[Double]](1) == Some(1.0)) }
+}
